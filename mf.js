d6f9b44898ed08d53e940ebc38b29ce38cc9d774
MFan <git@evlpeng.net>
2017-03-04 18:39:37 -0800

    now with portfolio

--- mf.js
+++ mf.js
@@ -4,7 +4,13 @@ $(function () {
     var camera, cameraTarget, scene, renderer;
 
     init();
+    initnav();
     animate();
+
+    $(window).scroll(function () {
+        clearTimeout($.data(this, 'scrollTimer'));
+        $.data(this, 'scrollTimer', setTimeout(pageCalc, 250));
+    });
 
     function init() {
 
@@ -125,4 +131,154 @@ $(function () {
         renderer.render(scene, camera);
     }
 
+    var orientation = {
+        TOP: 1,
+        BOTTOM: 2
+    };
+    var plist = [
+        { page: 1, anchor: orientation.TOP, offset: 0, element: 'canvas' },
+        { page: 2, anchor: orientation.TOP, offset: -4, element: '#work' },
+        { page: 3, anchor: orientation.BOTTOM, offset: 6, element: '#work li:eq(0) > div' },
+        { page: 4, anchor: orientation.BOTTOM, offset: 6, element: '#work li:eq(1) > div' },
+        { page: 5, anchor: orientation.BOTTOM, offset: 6, element: '#work li:eq(2) > div' },
+        { page: 6, anchor: orientation.BOTTOM, offset: 6, element: '#work li:eq(3) > div' },
+        { page: 7, anchor: orientation.BOTTOM, offset: 6, element: '#work li:eq(4) > div' },
+    ];
+    var page = 0;
+    function initnav() {
+        $("#cls-3").click(function () { pageto(1) });
+        $("#cls-2").click(function () { pageto(-1) });
+    }
+    function calcPlistPos(i) {
+        var dest = $(plist[i].element).offset().top;
+        if (plist[i].anchor == orientation.BOTTOM) {
+            dest += $(plist[i].element).outerHeight();
+            dest -= $(window).height();
+        }
+
+        //var offset = $(window).width() * (plist[i].offset / 100.0);
+        var offset = convertRem(plist[i].offset);
+        dest += offset;
+
+        return Math.round(dest);
+    }
+    function pageCalc() {
+        // calculate what page we're actually at
+        var x = 0;
+        while (x < plist.length - 1) {
+            if (calcPlistPos(x) >= $(window).scrollTop()) {
+                //if ($(window).scrollTop() !== calcPlistPos(x))
+                break;
+            }
+            ++x;
+        }
+
+        page = x - 1;
+
+        if (calcPlistPos(x) == $(window).scrollTop()) {
+            ++page;
+        }
+
+        if (page == plist.length - 2) {
+            if ($(window).scrollTop() > calcPlistPos(x)) {
+                ++page;
+            }
+        }
+
+        $("body").attr('data-page', page);
+        if (page == 0)
+            $("body").removeClass('page-last').addClass('page-first');
+        else if (page == (plist.length - 1))
+            $("body").removeClass('page-first').addClass('page-last');
+        else
+            $("body").removeClass('page-first').removeClass('page-last');
+
+    }
+    function pageto(amt) {
+        pageCalc();
+
+        var i = page + amt;
+
+        if (i < 0)
+            i = 0;
+        if (i > (plist.length - 1))
+            i = (plist.length - 1);
+
+        var dest = calcPlistPos(i);
+
+        $("html,body").animate(
+            {
+                scrollTop: dest
+            }, 700, "easeOutCirc"
+        );
+
+        page = i;
+
+}
+    function getRootElementFontSize() {
+        // Returns a number
+        return parseFloat(
+            // of the computed font-size, so in px
+            getComputedStyle(
+                // for the root <html> element
+                document.documentElement
+            )
+            .fontSize
+        );
+    }
+    function convertRem(value) {
+        return value * getRootElementFontSize();
+    }
+});
+
+$.easing.jswing = $.easing.swing;
+
+$.extend($.easing,
+{
+    def: 'easeOutQuad',
+    swing: function (x, t, b, c, d) {
+        //alert($.easing.default);
+        return $.easing[$.easing.def](x, t, b, c, d);
+    },
+    easeInCirc: function (x, t, b, c, d) {
+        return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
+    },
+    easeOutCirc: function (x, t, b, c, d) {
+        return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
+    },
+    easeInOutCirc: function (x, t, b, c, d) {
+        if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
+        return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
+    },
+    easeInOutElastic: function (x, t, b, c, d) {
+        var s = 1.70158; var p = 0; var a = c;
+        if (t == 0) return b; if ((t /= d / 2) == 2) return b + c; if (!p) p = d * (.3 * 1.5);
+        if (a < Math.abs(c)) { a = c; var s = p / 4; }
+        else var s = p / (2 * Math.PI) * Math.asin(c / a);
+        if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+        return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * .5 + c + b;
+    },
+    easeInOutBack: function (x, t, b, c, d, s) {
+        if (s == undefined) s = 1.70158;
+        if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
+        return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
+    },
+    easeInBounce: function (x, t, b, c, d) {
+        return c - $.easing.easeOutBounce(x, d - t, 0, c, d) + b;
+    },
+    easeOutBounce: function (x, t, b, c, d) {
+        if ((t /= d) < (1 / 2.75)) {
+            return c * (7.5625 * t * t) + b;
+        } else if (t < (2 / 2.75)) {
+            return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b;
+        } else if (t < (2.5 / 2.75)) {
+            return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b;
+        } else {
+            return c * (7.5625 * (t -= (2.625 / 2.75)) * t + .984375) + b;
+        }
+    },
+    easeInOutBounce: function (x, t, b, c, d) {
+        if (t < d / 2) return $.easing.easeInBounce(x, t * 2, 0, c, d) * .5 + b;
+        return $.easing.easeOutBounce(x, t * 2 - d, 0, c, d) * .5 + c * .5 + b;
+    }
 });
